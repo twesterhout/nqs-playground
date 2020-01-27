6b2015d9b7be162f0e9e602102634aff8ea49e3c
Tom Westerhout <t.westerhout@student.ru.nl>
2020-01-27 22:19:40 +0100

    Fix import order in __init__.py

--- nqs_playground/__init__.py
+++ nqs_playground/__init__.py
@@ -1,7 +1,6 @@
 # NOTE: We need to import PyTorch first, because we don't explicitly link
 # against it in C++ code.
 import torch as __torch
-unpack = __torch.ops.tcm.unpack
 
 from . import _C
 from ._C import SpinBasis
@@ -9,8 +8,5 @@ from .symmetry import *
 from .hamiltonian import *
 from .monte_carlo import *
 
-# from . import core
-# from . import hamiltonian
-# # from . import supervised
-# from . import symmetry
-# # from . import swo
+# This operator becomes available only after loading _C
+unpack = __torch.ops.tcm.unpack
