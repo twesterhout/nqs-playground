049553d18fbe394fce21c5e64ab4c0f730480fac
Tom Westerhout <t.westerhout@student.ru.nl>
2020-01-27 22:24:16 +0100

    Ran Black on setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,24 @@
-import glob
 from setuptools import setup
 
-setup(name='nqs_playground',
-      version='0.0.1',
-      description='PyTorch-based implementation of SR and SWO for NQS',
-      classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
-          'Environment :: Console',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 3 :: Only',
-          'Topic :: Scientific/Engineering :: Physics',
-      ],
-      url='http://github.com/twesterhout/nqs-playground',
-      author='Tom Westerhout',
-      author_email='t.westerhout@student.ru.nl',
-      license='BSD3',
-      packages=['nqs_playground'],
-      # This will break on OSX and Windows...
-      package_data={'nqs_playground': ['_C.*.so', 'libnqs.so']},
-      install_requires=[
-          # 'numpy',
-          # 'scipy',
-          # 'torch>=1.2',
-          # 'numba',
-          # 'tqdm',
-          # 'pytest',
-          # 'pytorch-ignite',
-          # 'mpi4py',
-      ],
-      zip_safe=False)
+setup(
+    name="nqs_playground",
+    version="0.0.1",
+    description="PyTorch-based implementation of SR and SWO for NQS",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    url="http://github.com/twesterhout/nqs-playground",
+    author="Tom Westerhout",
+    author_email="t.westerhout@student.ru.nl",
+    license="BSD3",
+    packages=["nqs_playground"],
+    # This will break on OSX and Windows...
+    package_data={"nqs_playground": ["_C.*.so", "libnqs.so"]},
+    install_requires=[],
+    zip_safe=False,
+)
