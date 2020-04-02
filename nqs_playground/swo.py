ab0cf001d6295c20fdf279fb4320a0988e6d2448
Tom Westerhout <t.westerhout@student.ru.nl>
2020-04-02 20:56:50 +0200

    Removed some old code & temporary disabled test datasets

--- nqs_playground/swo.py
+++ nqs_playground/swo.py
@@ -127,390 +127,17 @@ Config = namedtuple(
 )
 
 
-def _create_sign_trainer(
-    model: torch.nn.Module, optimiser: torch.optim.Optimizer
-) -> ignite.engine.Engine:
-    r"""Creates an engine for training the network on the sign structure.
-
-    Cross entropy is used as loss function.
-
-    :param model: PyTorch model to train. Given a ``torch.FloatTensor`` of
-        shape ``(batch_size, num_spins)``, ``model`` must return a
-        ``torch.FloatTensor`` of shape ``(batch_size, 2)``. Columns of the
-        output tensor are interpreted as unnormalised probabilities of spin
-        configurations having signs ``+1`` or ``-1``.
-    :param optimiser: PyTorch optimiser to use.
-    """
-    loss_fn = CrossEntropyLoss()
-
-    def _update(engine, batch):
-        # print(engine.state)
-        model.train()
-        optimiser.zero_grad()
-        x, y = batch
-        y_pred = model(x)
-        loss = loss_fn(y_pred, y)
-        loss.backward()
-        optimiser.step()
-        return loss.item()
-
-    return ignite.engine.Engine(_update)
-    # return create_supervised_trainer(model, optimiser, loss_fn=CrossEntropyLoss())
-
-
-def _create_sign_evaluator(model: torch.nn.Module) -> ignite.engine.Engine:
-    r"""Creates an engine for evaluating how well the network learned the sign
-    structure.
-
-    Evaluator computes two metrics: accuracy and cross entropy loss. Accuracy
-    is simply the percentage of signs that the model predicted correctly. Cross
-    entropy loss is also averaged over all the samples.
-
-    :param model: PyTorch model. Given a ``torch.FloatTensor`` of
-        shape ``(batch_size, num_spins)``, ``model`` must return a
-        ``torch.FloatTensor`` of shape ``(batch_size, 2)``. Columns of the
-        output tensor are interpreted as unnormalised probabilities of spin
-        configurations having signs ``+1`` or ``-1``.
-    """
-    return create_supervised_evaluator(
-        model,
-        metrics={
-            "cross_entropy": ignite.metrics.Loss(torch.nn.CrossEntropyLoss()),
-            "accuracy": ignite.metrics.Accuracy(
-                lambda output: (torch.argmax(output[0], dim=1), output[1])
-            ),
-        },
-    )
-
-
-def _create_amplitude_trainer(
-    model: torch.nn.Module, optimiser: torch.optim.Optimizer
-) -> ignite.engine.Engine:
-    r"""Creates an engine for training the network on wavefunction amplitudes.
-
-    The model is trained to maximise the overlap with the target state.
-
-    :param model: PyTorch model. Given a ``torch.FloatTensor`` of
-        shape ``(batch_size, num_spins)``, ``model`` must return a
-        ``torch.FloatTensor`` of shape ``(batch_size, 1)``. It is interpreted
-        as **wavefunction amplitudes** (NOTE: no logarithms!).
-    :param optimiser: PyTorch optimiser.
-    """
-
-    # The following loss function closely resembles something what one would
-    # write if we were simply trying to compute overlap from Monte Carlo data.
-    @torch.jit.script
-    def loss_fn(predicted, expected):
-        assert torch.all(predicted >= 0) and torch.all(expected >= 0)
-        predicted = predicted.squeeze()
-        expected = expected.squeeze()
-        quotient = expected / predicted
-        return 1 - torch.sum(quotient) ** 2 / torch.norm(
-            quotient
-        ) ** 2 / predicted.size(0)
-
-    # @torch.jit.script
-    # def loss_fn(predicted: torch.Tensor, expected: torch.Tensor):
-    #     predicted = predicted.squeeze()
-    #     return (
-    #         1
-    #         - torch.dot(predicted, expected) ** 2
-    #         / torch.norm(predicted) ** 2
-    #         / torch.norm(expected) ** 2
-    #     )
-
-    return create_supervised_trainer(model, optimiser, loss_fn=loss_fn)
-
-
-def _create_amplitude_evaluator(model: torch.nn.Module) -> ignite.engine.Engine:
-    r"""Creates an engine for evaluating how well the network learned
-    wavefunction amplitude.
-    """
-    return create_supervised_evaluator(
-        model,
-        metrics={
-            "weighted_fidelity": FidelityMetric(weighted=True),
-            "fidelity": FidelityMetric(weighted=False),
-        },
-    )
-
-
-def create_trainer(
-    target: str, model: torch.nn.Module, optimiser: torch.optim.Optimizer
-) -> ignite.engine.Engine:
-    r"""Creates an engine for optimising either the amplitude or the sign of the
-    wavefunction.
-
-    :param target: either "amplitude" or "sign".
-    :param model: PyTorch model to train.
-    :param optimiser: PyTorch optimiser to use for training.
-    """
-    return {"sign": _create_sign_trainer, "amplitude": _create_amplitude_trainer}[
-        target
-    ](model, optimiser)
-
-
-def create_evaluators(target: str, model: torch.nn.Module) -> ignite.engine.Engine:
-    r"""Creates an engine for evaluating the performance of the network trained
-    on either amplitude or sign of the wavefunction.
-
-    :param target: either "amplitude" or "sign".
-    :param model: PyTorch model.
-    """
-    make = {"sign": _create_sign_evaluator, "amplitude": _create_amplitude_evaluator}[
-        target
-    ]
-    return namedtuple("Evaluators", ["training", "validation"])(
-        make(model), make(model)
-    )
-
-
-class OverlapMetric(ignite.metrics.metric.Metric):
-    r"""An Ignite metric for computing overlap of with the target state.
-    """
-
-    def __init__(self, output_transform=lambda x: x, weighted=True):
-        self._dot = None
-        self._norm_predicted = None
-        self._norm_expected = None
-        self._weighted = weighted
-        super().__init__(output_transform=output_transform)
-
-    def reset(self):
-        self._dot = 0
-        self._norm_predicted = 0
-        self._norm_expected = 0
-
-    def update(self, output):
-        predicted, expected = output
-        predicted = predicted.squeeze()
-        expected = expected.squeeze()
-        assert torch.all(predicted >= 0) and torch.all(expected >= 0)
-        if self._weighted:
-            self._dot += torch.dot(predicted, expected).item()
-            self._norm_predicted += torch.norm(predicted).item() ** 2
-            self._norm_expected += torch.norm(expected).item() ** 2
-        else:
-            quotient = expected / predicted
-            self._dot += torch.sum(quotient).item()
-            self._norm_predicted += predicted.size(0)
-            self._norm_expected += torch.norm(quotient).item() ** 2
-
-    def compute(self):
-        if self._norm_expected == 0:
-            raise ignite.exceptions.NotComputableError(
-                "OverlapMetric must have at least one example before it can be computed."
-            )
-        return abs(self._dot) / sqrt(self._norm_predicted * self._norm_expected)
-
-
-class FidelityMetric(OverlapMetric):
-    def __init__(self, output_transform=lambda x: x, weighted=True):
-        super().__init__(output_transform=output_transform, weighted=weighted)
-
-    def compute(self):
-        return 1.0 - super().compute() ** 2
-
-
-class Trainer:
-    r"""Class which implements supervised learning.
-    """
-
-    def __init__(
-        self,
-        target: str,
-        model: torch.nn.Module,
-        dataset: Tuple[np.ndarray, torch.Tensor],
-        basis,
-        output: str,
-        config: TrainingOptions,
-    ):
-        r"""Initialises the trainer.
-
-        :param target: what are we learning? Either "amplitude" or "sign".
-        :param model: model to train. Must predict either signs or log
-            amplitudes.
-        :param dataset: a pair of inputs and outputs constituting the full
-            dataset available to us for training. Trainer automatically splits
-            it into training and validation datasets.
-        :param basis: Hilbert space basis.
-        :param output: directory where to save log files and model weights.
-        :param config: training hyperparameters such as learning rate, fraction
-            of the dataset to use for validation etc.
-        """
-        self.target = target
-        self.output = output
-        self.config = config
-        self.prefix = "best"
-        self.basis = basis
-        self.__add_model(model, dataset)
-        self.optimiser = config.optimiser(self.model.parameters())
-        self.__add_loaders(dataset)
-        self.__add_engines()
-        self.__add_loggers()
-        self.__add_handlers()
-
-    def __add_model(self, model, dataset):
-        print("__add_model")
-        if self.target == "sign":
-            self.model = model
-        else:
-
-            # Since we're optimising overlap and model predicts log amplitudes,
-            # we first transform it to predict actual amplitudes. Effectively
-            # we just want to add a new layer taking the exponent, but this can
-            # overflow. So instead we first determine the maximal predicted
-            # value in the dataset and then rescale the output to prevent
-            # overflows.
-            class _Model(torch.nn.Module):
-                __constants__ = ["scale"]
-
-                def __init__(self, module, scale):
-                    super().__init__()
-                    self.module = module
-                    self.scale = scale
-
-                def forward(self, x):
-                    return torch.exp(self.module(x) - self.scale)
-
-            with torch.no_grad(), torch.jit.optimized_execution(True):
-                xs, _ = dataset
-                ys = forward_with_batches(lambda x: model(x), xs, batch_size=8192)
-                scale = float(torch.max(ys))  # - 5.0
-            self.original_model = model
-            self.model = torch.jit.script(_Model(model, scale))
-
-    def __add_loaders(self, dataset: Tuple[np.ndarray, torch.Tensor]):
-        with torch.no_grad():
-            # Let us transform the whole dataset now once so that we don't have
-            # to do it on a per-batch basis.
-            x, y = dataset
-            if self.target == "sign":
-                y = (y < 0).to(torch.long)
-            else:
-                y = torch.abs(y).squeeze()
-            dataset = (x, y)
-
-        Loaders = namedtuple("Loaders", ["training", "validation"], defaults=[None])
-        # Special case when we don't want to use validation dataset
-        # if self.config.train_fraction == 1.0:
-        self.loaders = Loaders(
-            SpinDataset(
-                *dataset, shuffle=False, batch_size=self.config.train_batch_size
-            )
-        )
-
-    def __add_engines(self):
-        self.trainer = create_trainer(self.target, self.model, self.optimiser)
-        # self.evaluators = create_evaluators(self.target, self.model)
-
-    def __add_loggers(self):
-        self.tb_writer = SummaryWriter(log_dir=self.output)
-        # self.tqdm_writer = ignite.contrib.handlers.tqdm_logger.ProgressBar()
-
-    def __add_handlers(self):
-        # self.tqdm_writer.attach(
-        #     self.trainer,
-        #     event_name=Events.EPOCH_COMPLETED,
-        #     closing_event_name=Events.COMPLETED,
-        # )
-        # self.__add_evaluators()
-        # self.__add_checkpoint()
-        # self.__add_early_stopping()
-        pass
-
-    def __add_evaluators(self):
-        def add(tag, evaluator, dataloader):
-            def log_results(engine):
-                n = engine.state.epoch
-                evaluator.run(dataloader)
-                for name, value in evaluator.state.metrics.items():
-                    print("{}/{}: {}: {}".format(tag, name, n, value))
-                    self.tb_writer.add_scalar("{}/{}".format(tag, name), value, n)
-
-            self.trainer.add_event_handler(Events.STARTED, log_results)
-            self.trainer.add_event_handler(Events.EPOCH_COMPLETED, log_results)
-
-        add("training", self.evaluators.training, self.loaders.training)
-        # if self.loaders.validation is not None:
-        #     add("validation", self.evaluators.validation, self.loaders.validation)
-
-    # def __score_fn(self):
-    #     if self.target == "sign":
-    #         return lambda e: -e.state.metrics["cross_entropy"]
-    #     else:
-    #         return lambda e: -e.state.metrics["fidelity"]
-
-    # def __add_checkpoint(self):
-    #     evaluator = (
-    #         self.evaluators.validation
-    #         if self.loaders.validation is not None
-    #         else self.evaluators.training
-    #     )
-    #     evaluator.add_event_handler(
-    #         Events.COMPLETED,
-    #         ignite.handlers.ModelCheckpoint(
-    #             dirname=self.output,
-    #             filename_prefix=self.prefix,
-    #             score_function=self.__score_fn(),
-    #             require_empty=True,
-    #             n_saved=1,
-    #             save_as_state_dict=True,
-    #         ),
-    #         {"model": self.model},
-    #     )
-
-    # def load_best(self):
-    #     r"""Loads back weights which achieved the best score during training."""
-    #     matched = [f for f in os.listdir(self.output) if f.startswith(self.prefix)]
-    #     if len(matched) == 0:
-    #         raise ValueError(
-    #             "Could not load the checkpoint. There are no files "
-    #             "prefixed with {!r} in the directory {!r}."
-    #             "".format(self.prefix, self.output)
-    #         )
-    #     if len(matched) > 1:
-    #         raise ValueError(
-    #             "Could not load the checkpoint. There are more than "
-    #             "one files prefixed with {!r} in the directory {!r}."
-    #             "".format(self.prefix, self.output)
-    #         )
-    #     matched = next(iter(matched))
-    #     self.model.load_state_dict(torch.load(os.path.join(self.output, matched)))
-
-    # def __add_early_stopping(self):
-    #     self.evaluators.validation.add_event_handler(
-    #         Events.COMPLETED,
-    #         ignite.handlers.EarlyStopping(
-    #             patience=self.config.patience,
-    #             score_function=self.__score_fn(),
-    #             trainer=self.trainer,
-    #         ),
-    #     )
-
-    def run(self):
-        self.trainer.run(self.loaders.training, max_epochs=self.config.max_epochs)
-        # self.load_best()
-        # if self.target == "amplitude":
-        #     # Since we're transformed the model, it's a good idea to check that
-        #     # we've actually updated the weights in the original model as well
-        #     state_dict = self.model.state_dict()
-        #     for name, tensor in self.original_model.state_dict().items():
-        #         assert torch.all(tensor == state_dict["module." + name])
-
-
-def train(target, model, dataset, basis, output, config):
-    r"""Runs supervised training."""
-    Trainer(target, model, dataset, basis, output, config).run()
-
-
-@torch.jit.script
+# @torch.jit.script
 def overlap_loss_fn(
     predicted: Tensor, expected: Tensor, weights: Tensor  # ] = None
 ) -> Tensor:
     r"""
         ∑ψφ = 10
     """
+    if not torch.all(predicted >= 0):
+        print("predicted: ", predicted)
+    if not torch.all(expected >= 0):
+        print("expected: ", expected)
     assert torch.all(predicted >= 0) and torch.all(expected >= 0)
     predicted = predicted.squeeze()
     expected = expected.squeeze()
@@ -953,6 +580,8 @@ class Runner(object):
         if self.exact is None:
             return None
 
+        self.amplitude.eval()
+        self.sign.eval()
         with torch.no_grad():
             basis = self.config.hamiltonian.basis
             y = forward_with_batches(
@@ -998,6 +627,8 @@ class Runner(object):
         tqdm.write("Monte Carlo sampling from |ψ|²...", end="")
         start = time.time()
 
+        self.amplitude.eval()
+        self.sign.eval()
         spins, _, _ = sample_some(
             self.amplitude,
             self.config.hamiltonian.basis,
@@ -1044,7 +675,7 @@ class Runner(object):
             self.compute_statistics()
 
         dataset = self.monte_carlo()
-        test_dataset = self.prepare_test_dataset()
+        test_dataset = None # self.prepare_test_dataset()
         train_amplitude(
             self.amplitude,
             dataset,
