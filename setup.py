a8ad23115ec6350a4a9fd5ba6556926ca318b6dc
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-06-13 20:07:49 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nqs_playground",
-    version="0.0.9",
+    version="0.0.10",
     description="PyTorch-based implementation of SR and SWO for NQS",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
