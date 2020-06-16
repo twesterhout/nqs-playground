cd6e6e2c531aeecdaacf8126aaa8a07b35b97260
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-06-16 13:06:06 +0200

    Proper unpack tests

--- test/test_unpack.py
+++ test/test_unpack.py
@@ -1,30 +1,49 @@
+import numpy as np
 import torch
-from torch import Tensor
-
 from nqs_playground import *
 
+np.random.seed(52339877)
+torch.manual_seed(9218823294)
+manual_seed(3362121853)
 
 
-def unpack_simple(spins: Tensor, n: int) -> Tensor:
-
-    def _unpack_one(bits: int) -> Tensor:
-        out = torch.empty(n, dtype=torch.float32)
-        for i in range(n):
-            out[i] = float(2 * ((bits >> i) & 1) - 1)
-        return out
+@torch.no_grad()
+def unpack_complete(spins):
+    def unpack_one(bits, out):
+        bits = bits.numpy().view(np.uint64)
+        for i in range(8):
+            word = int(bits[i])
+            for j in range(64):
+                out[64 * i + j] = float(2 * ((word >> j) & 1) - 1)
 
     device = spins.device
     spins = spins.cpu()
-    return torch.stack([_unpack_one(x) for x in spins]).to(device)
+    out = torch.empty(spins.size(0), 512, dtype=torch.float32)
+    for i in range(spins.size(0)):
+        unpack_one(spins[i], out[i])
+    return out.to(device)
 
 
+@torch.no_grad()
+def generate(batch_size, device):
+    data = np.random.default_rng().integers(
+        1 << 64 - 1, size=(batch_size, 8), dtype=np.uint64, endpoint=True
+    )
+    data = torch.from_numpy(data.view(np.int64)).clone().to(device)
+    return data
+
+
+@torch.no_grad()
 def test_unpack(device):
-    ns = torch.randint(1, 64, (10,))
-    for n in map(lambda x: x.item(), ns):
-        spins = torch.randint(0, (1<<n), (587,)).to(device)
-        predicted = unpack(spins, n)
-        expected = unpack_simple(spins, n)
-        assert (predicted == expected).all()
+    for batch_size in [1, 5, 128, 437]:
+        packed = generate(batch_size, device)
+        full = unpack_complete(packed)
+        for number_spins in [1, 2, 32, 64, 65, 400]:
+            predicted = unpack(packed, number_spins)
+            expected = full[:, :number_spins]
+            assert (predicted == expected).all()
 
-test_unpack('cuda')
-test_unpack('cpu')
+
+test_unpack("cpu")
+if torch.cuda.is_available():
+    test_unpack("cuda")
