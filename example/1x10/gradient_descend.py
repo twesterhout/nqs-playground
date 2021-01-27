88d438a8c7796e52e9c0be6716c23165976c28d7
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-01-28 00:25:10 +0100

    SGD+Momentum and small batch size

--- example/1x10/gradient_descend.py
+++ example/1x10/gradient_descend.py
@@ -10,12 +10,13 @@ torch.manual_seed(9218823294)
 import nqs_playground
 import nqs_playground.sgd
 import nqs_playground.autoregressive
+
 # except ImportError:
 #     # For local development when we only compile the C++ extension, but don't
 #     # actually install the package using pip
 #     import os
 #     import sys
-# 
+#
 #     sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", ".."))
 #     import nqs_playground
 
@@ -57,23 +58,43 @@ def main():
     # fmt: on
     edges = [(i, (i + 1) % basis.number_spins) for i in range(basis.number_spins)]
     operator = ls.Operator(basis, [ls.Interaction(matrix, edges)])
-    E, ground_state = ls.diagonalize(operator)
-    logger.info("E = {}", E)
-    logger.info("v₀ = {}", ground_state[:, 0])
+    # E, ground_state = ls.diagonalize(operator)
+    # logger.info("E = {}", E)
+    # logger.info("v₀ = {}", ground_state[:, 0])
 
+    amplitude = make_amplitude("nade" if use_autoregressive else "dense")
+    phase = make_phase()
+    optimizer = torch.optim.SGD(
+        list(amplitude.parameters()) + list(phase.parameters()),
+        lr=1e-1,
+        momentum=0.9,
+        weight_decay=1e-4,
+    )
+    # optimizer = torch.optim.Adam(list(amplitude.parameters()) + list(phase.parameters()), lr=1e-2)
     options = nqs_playground.sgd.Config(
-        model=[make_amplitude("nade" if use_autoregressive else "dense"), make_phase()],
-        output="1x10.result",
-        exact=ground_state[:, 0],
+        amplitude=amplitude,
+        phase=phase,
         hamiltonian=operator,
-        epochs=300,
-        sampling_options=nqs_playground.SamplingOptions(number_samples=20000, number_chains=1),
+        output="1x10.result",
+        epochs=600,
+        sampling_options=nqs_playground.SamplingOptions(number_samples=128, number_chains=1),
         sampling_mode="autoregressive" if use_autoregressive else "exact",
-        optimiser=lambda p: torch.optim.Adam(p, lr=1e-2),
+        exact=None,  # ground_state[:, 0],
+        constraints={"hamming_weight": lambda i: 0.1},
+        optimizer=optimizer,
+        inference_batch_size=8192,
     )
     runner = nqs_playground.sgd.Runner(options)
     for i in range(options.epochs):
         runner.step()
+        if runner._epoch % 200 == 0:
+            for g in options.optimizer.param_groups:
+                g["lr"] /= 2
+                g["momentum"] /= 2
+    runner.config = options._replace(
+        sampling_options=nqs_playground.SamplingOptions(number_samples=200000, number_chains=1),
+    )
+    runner.step()
 
 
 if __name__ == "__main__":
