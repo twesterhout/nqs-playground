0bc36d2e84a5d7b0864b0d29723564695a346625
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-02-18 19:09:43 +0100

    Script to build locally in CPU-only mode

---
+++ build_locally_cpu.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env -c bash
+
+set -eu
+set -o pipefail
+
+get_repo_root() {
+  git rev-parse --show-toplevel
+}
+
+activate_environment() {
+  if ! conda env list | grep -q nqs_devel_cpu; then
+    echo "You do not have nqs_devel_cpu Conda environment. Creating it..."
+    conda env create --file conda-cpu.yml
+  fi
+  echo "Activating nqs_devel_cpu environment..."
+  conda activate nqs_devel_cpu
+}
+
+build_cxx_code() {
+  echo "Building C++ extension code..."
+  mkdir -vp build
+  pushd build
+  declare -r site_packages_dir=$(python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
+  cmake -GNinja \
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS -march=nehalem" \
+    -DCMAKE_C_FLAGS="$CFLAGS -match=nehalem" \
+    -DCMAKE_PREFIX_PATH="$site_packages_dir/torch/share/cmake" \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+  cmake --build . --target install
+  popd
+  echo "Done building C++ code!"
+}
+
+install_python_package() {
+  echo "Installing Python package..."
+  python3 -m pip install -e .
+}
+
+main() {
+  cd "$(get_repo_root)"
+  activate_environment
+  build_cxx_code
+  install_python_package
+}
