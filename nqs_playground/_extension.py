de7249cdb0b098943903b140fe5bdabc07e4c6a1
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-09-27 13:52:40 +0200

    Extension loading

---
+++ nqs_playground/_extension.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import torch
+from torch.utils.cpp_extension import load as _load
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+lib = _load(
+    "_nqs_playground_cpp",
+    [os.path.join(PACKAGE_DIR, "cbits", "zanella.cpp"),
+     os.path.join(PACKAGE_DIR, "cbits", "init.cpp")
+     ],
+    extra_cflags=[
+        "-fvisibility=hidden",
+        "-std=c++17",
+        "-g",
+        "-march=native",
+        "-O3",
+        "-ftree-vectorize",
+    ],
+    extra_include_paths=[os.path.join(sys.prefix, "include")],
+    extra_ldflags=["-L" + os.path.join(sys.prefix, "lib"), "-llattice_symmetries"],
+    verbose=True
+)
+
